88204f5801dc51cc6305ba4e4f8cfd6411661fdd
pablofrizzi <pablofrizzi@gmail.com>
2017-04-25 20:22:39 +0200

    loadData divided to multiple functions

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 $(document).ready(main);
 
 function main() {
-    $('#form-container').submit(loadData);
+    $('#form-container').on('submit', loadData);
 }
-
-function loadData() {
 
     var body = $('body');
     var wikiEl = $('#wikipedia-links');
     var nytHeaderEl = $('#nytimes-header');
     var nytEl = $('#nytimes-articles');
     var greeting = $('#greeting');
-    
+    var img = $('.bgimg');
+
+
+function dataClear() {
     // clear out old data before new request
-    wikiEl.text("");
-    nytEl.text("");
-    
-    // Google Maps request
-    
+    wikiEl.text('');
+    nytEl.text('');
+}
+
+function prepareGoogleData() {
     var streetName = $('#street').val();
     var cityName = $('#city').val();
     var address = streetName + ', ' + cityName;
@@ -26,17 +27,18 @@ function loadData() {
     
     var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '';
     
-    body.append('<img class="bgimg" src=" ' + streetViewUrl + ' ">');
-    
-    // NY Times AJAX request
-    
+    img.attr("src", streetViewUrl); 
+}
+
+function prepareNYTimesData() {
+    var cityName = $('#city').val();
     var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityName + '&sort=oldest&api-key=3c2b347976404869946e1dcc15e46b03' ;
     
     $.getJSON(nytimesUrl, function(data){
        
         nytHeaderEl.text('New York Times Articles about ' + cityName);
         
-        articles = data.response.docs;
+        var articles = data.response.docs;
         for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
             nytEl.append('<li class="article">'+
@@ -45,9 +47,10 @@ function loadData() {
     }).error(function(e){
         nytHeaderEl.text('New York Times Article Could Not Be Loaded');
     });
-    
-    // Wikipedia AJAX request
-    
+}
+
+function prepareWikiData() {
+    var cityName = $('#city').val();
     var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityName + '&format=json&callback=wikiCallback';
     
     $.ajax({
@@ -64,9 +67,27 @@ function loadData() {
             };
         }
         
-    })
+    });
+}
+
+function loadData(event) {
     
-    return false;
+    event.preventDefault();
+        
+    dataClear();
+    
+    // Google Maps request
+    
+    prepareGoogleData();
+    
+    // NY Times AJAX request
+    
+    prepareNYTimesData();
+    
+    // Wikipedia AJAX request
+    
+    prepareWikiData();
+    
 };
 
 
